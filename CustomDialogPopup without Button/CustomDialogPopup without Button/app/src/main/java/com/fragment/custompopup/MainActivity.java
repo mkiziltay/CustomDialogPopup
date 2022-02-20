e91a8b824547ff2be442e61e920eff08c51a1397
Mustafa KIZILTAY <85419797+mkiziltay@users.noreply.github.com>
2022-02-21 01:31:26 +0300

    Add files via upload

---
+++ CustomDialogPopup without Button/CustomDialogPopup without Button/app/src/main/java/com/fragment/custompopup/MainActivity.java
@@ -0,0 +1,35 @@
+package com.fragment.custompopup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button popup;
+    Dialog mDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        popup = findViewById(R.id.showPopup);
+        mDialog = new Dialog(this);
+
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDialog.setContentView(R.layout.popup_layout);
+                mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                mDialog.show();
+            }
+        });
+    }
+}
